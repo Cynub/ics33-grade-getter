42109349a68f96d4b115273d25ad2f2c66cebd52
Cynub <isaacalaniz85@gmail.com>
2022-03-13 16:07:23 -0700

    Changed download_grades
    Returns a Path object now

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import urllib.request
 import tempfile
+from pathlib import Path
 
 GRADE_URL = 'https://www.ics.uci.edu/~pattis/ICS-33/ics33win22grades.zip'
 
-def download_grades() -> str:
+def download_grades() -> Path:
     request = urllib.request.Request(GRADE_URL)
     response = urllib.request.urlopen(request)
     grades = response.read()
     response.close()
-    temp_file = tempfile.NamedTemporaryFile(mode='wb')
-    with temp_file as f:
+    grades_path = Path(tempfile.gettempdir())/'grades.zip'
+    with open(grades_path, 'wb') as f:
         f.write(grades)
-        return temp_file.name
+    return grades_path
 
 
 if __name__ == '__main__':
